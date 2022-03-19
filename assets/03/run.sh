1f5648537cccc59b46fefb4bd50e33ae53684bfc
car <carlos.echc11@gmail.com>
2022-03-19 01:06:09 -0600

    03

--- assets/03/run.sh
+++ assets/03/run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -x
-kubectl apply -f assets/17/headless-svc.yml
-kubectl apply -f assets/17/local-sc.yml
-kubectl apply -f assets/17/data-pv.yml
-kubectl apply -f assets/17/mongoconf-cm.yml
-kubectl apply -f assets/17/mongodbkey-secret.yml
-kubectl apply -f assets/17/mongodbadmin-secret.yml
-kubectl apply -f assets/17/mongodb-sts.yml
+kubectl apply -f assets/03/headless-svc.yml
+kubectl apply -f assets/03/local-sc.yml
+kubectl apply -f assets/03/data-pv.yml
+kubectl apply -f assets/03/mongoconf-cm.yml
+kubectl apply -f assets/03/mongodbkey-secret.yml
+kubectl apply -f assets/03/mongodbadmin-secret.yml
+kubectl apply -f assets/03/mongodb-sts.yml
 # kubectl exec mongodb-2 -- mongo admin -u admin -p admin --eval "rs.status().members.forEach(function(z){printjson(z.name);printjson(z.stateStr);})"
 watch kubectl get pods -l app=mongodb
